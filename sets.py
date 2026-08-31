8aefaa512ed08a7920bfa158849294513857eabb
0-Mayday-0 <mateogreslebin@gmail.com>
2026-08-30 21:10:23 -0300

    First working approach
    Used a simple assertion try/except to cancel the last delay of a line for easier block fitting

--- sets.py
+++ sets.py
@@ -14,22 +14,32 @@ class CharacterSet:
     def __iter__(self):
         yield from self.yield_delay()
 
+    def __len__(self) -> int:
+        return len(self.characters)
 
 class Line:
     def __init__(self, sets: list[CharacterSet], delay_between: float, cancel_last_delay: bool = False) -> None:
-        self.sets = sets
-        self.delay_between = delay_between
+        self._sets = sets
+        self._delay_between = delay_between
+        self._cancel_last_delay = cancel_last_delay
 
     def print_delay(self):
-        for st in self.sets:
-            for i in st:
-                print(i, end='', flush=True)
+        len_sets: int = len(self._sets) - 1
+        for i, st in enumerate(self._sets):
+            local_index: int = i
+            for i, v in enumerate(st):
+                print(v, end='', flush=True)
 
+            try:
+                assert local_index < len_sets
+                tsleep(self._delay_between)
 
-
+            except AssertionError:
+                if self._cancel_last_delay:
+                    break
 
 def main() -> None:
-    a = Line([CharacterSet("Hello ", 0.2), CharacterSet("World", 0.5)])
+    a = Line([CharacterSet("Hello ", 0.2), CharacterSet("World", 0.5)], 5)
 
     a.print_delay()
 
