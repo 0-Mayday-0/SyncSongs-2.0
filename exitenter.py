e1bd4ba10dc4523803421734da4144f1a3be152a
0-Mayday-0 <mateogreslebin@gmail.com>
2026-08-31 12:23:50 -0300

    Worked on the song + small updates
    Worked on the intro of the song and made example data structures to show how to store the Line object

--- exitenter.py
+++ exitenter.py
@@ -5,17 +5,33 @@ from time import sleep as tsleep
 
 class ExitEnter(Song):
     def __init__(self) -> None:
-        self._default_delay_intro: int | float = 0.1
-        self._intro_half_time: int | float = self._default_delay_intro-0.05
-        self._default_wait_lines_intro = 2
+        self._intro_delays: dict[str, int | float] = {}
 
-        self._intro_line0: Line = Line([CharacterSet("The sun", self._default_delay_intro),
-                                              CharacterSet(" says leave\n", self._intro_half_time),
-                                              CharacterSet("The clock", self._default_delay_intro),],
-                                            self._default_wait_lines_intro, cancel_last_delay=True)
+        self._intro_delays['intro default delay'] = 0.1
+        self._intro_delays['intro half time'] = self._intro_delays['intro default delay']-0.05
+        self._intro_delays['intro default wait lines'] = 2
+        self._intro_delays['intro swing'] = self._intro_delays['intro default wait lines']-0.25
+        self._intro_delays['intro thirds'] = self._intro_delays['intro swing']-0.05
+        self._intro_delays['intro slightly slower'] = self._intro_delays['intro half time']+0.03
 
-        self._intro_line1: Line = Line([CharacterSet(" agrees\n", self._default_delay_intro)],
-                                       self._default_wait_lines_intro)
+        self._intro_lines: list[Line | float | int] = [Line([CharacterSet("The sun", self._intro_delays['intro default delay']),
+                                                                  CharacterSet(" says leave\n", self._intro_delays['intro half time']),
+                                                                  CharacterSet("The clock", self._intro_delays['intro default delay']),],
+                                                self._intro_delays['intro default wait lines'], cancel_last_delay=True),
+
+                                                       self._intro_delays['intro thirds'],
+
+                                                       Line([CharacterSet(" agrees\n", self._intro_delays['intro default delay']),
+                                                                  CharacterSet("My dreams", self._intro_delays['intro half time']),
+                                                                  CharacterSet(" have left", self._intro_delays['intro slightly slower']),
+                                                                ],
+                                                            self._intro_delays['intro swing']),
+
+                                                       self._intro_delays['intro swing'],
+
+                                                       Line([CharacterSet(" ahead", self._intro_delays['intro default delay']),
+                                                            CharacterSet(" of me\n", self._intro_delays['intro default delay'])]
+
 
     @staticmethod
     def _async_sound(path) -> None:
@@ -29,11 +45,11 @@ class ExitEnter(Song):
 
         tsleep(local_intro_delay)
 
-        self._intro_line0.print_delay()
-
-        tsleep(local_intro1_delay)
-
-        self._intro_line1.print_delay()
+        for line in self._intro_lines:
+            try:
+                line.print_delay()
+            except AttributeError:
+                tsleep(line)
 
         tsleep(1) #debug
 
